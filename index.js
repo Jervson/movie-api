269a7a7eae8671a2a82bae2897093b5239a8643f
Opilane TTHK <opilane@tthk.ee>
2023-10-05 09:52:47 +0300

    add to index

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require ('swagger-ui-express')
 const swaggerDocument = require ('./docs/swagger.json')
 
+app.get('/movies', (req, res) => {
+    req.send(["Shark Attack", "Shark Attack 4"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
