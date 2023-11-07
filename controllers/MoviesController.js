ed2b8364f028a4bc107806dd04082a3132c012fd
jervson <henri.jervson@gmail.com>
2023-11-07 12:41:03 +0200

    frontend

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -16,7 +16,7 @@ exports.createNew = async (req, res) => {
 }
 // READ
 exports.getAll = async (req, res) => {
-    const result = await movies.findAll({ attributes: ["id", "name"] })
+    const result = await movies.findAll({ attributes: ["id", "name", "description"] })
     res.json(result)
 }
 exports.getById = async (req, res) => {
